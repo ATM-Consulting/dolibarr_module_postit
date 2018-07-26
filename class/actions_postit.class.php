983a28c2caa242beb0b5e0b6987f8b29f06dd85d
gauthier <gauthier.verdol@atm-consulting.fr>
2018-07-26 15:06:42 +0200

    FIX : pour une meilleure compréhension

--- class/actions_postit.class.php
+++ class/actions_postit.class.php
@@ -61,7 +61,7 @@ class Actionspostit
 			$form=new Form($db);
 			$select_user = $form->select_dolusers($user->id, 'fk_user', 1);
 		
-			$a = '<a id="addNote" href="javascript:createNote(0)" style="position:absolute; '.($conf->theme === 'eldy' ? 'left:-30px;' : '').' top:0px; display:block;">'.img_picto('', 'post-it.png@postit',' style="width:32px; height:32px;" ').'</a>';
+			$a = '<a id="addNote" href="javascript:createNote(0)" style="position:absolute; '.($conf->theme !== 'eldy' ? '' : 'left:-30px;').' top:0px; display:block;">'.img_picto('', 'post-it.png@postit',' style="width:32px; height:32px;" ').'</a>';
 			
 			$aDelete =' <span rel="delete">'.img_delete().'</span>';
 			$aResponse =' <span rel="response">'.img_picto('','response.png@postit').'</span>';
@@ -203,7 +203,7 @@ class Actionspostit
 					<?php if($conf->theme !== 'eldy') print '$div.css("z-index", 100);' ?>
 					$div.css('height', 200);
 					$div.css('top', pos.top + 20);
-					$div.css('left', pos.left <?php $conf->theme === 'eldy' ? print '-50' : ''; ?>);   
+					$div.css('left', pos.left <?php $conf->theme !== 'eldy' ? print '' : '-50'; ?>);   
 					
 					
 					$('body').append($div);
