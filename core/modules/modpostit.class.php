b89f567991bb0db06274af172a88180893d7cbfb
John BOTELLA <john.botella@atm-consulting.fr>
2022-06-01 12:14:46 +0200

    Fix wrong hook usage

--- core/modules/modpostit.class.php
+++ core/modules/modpostit.class.php
@@ -60,7 +60,7 @@ class modpostit extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Permet d'ajouter sur les fiches Dolibarr des notes repositionnables, privées ou publiques, qui attirent l'œil";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.5.3';
+		$this->version = '1.5.4';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
